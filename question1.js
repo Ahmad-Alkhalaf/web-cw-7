b89fddbca56e46f32deb07b3ede7e8994a7e3135
Ahmad-Alkhalaf <76259161+Ahmad-Alkhalaf@users.noreply.github.com>
2021-01-05 21:26:46 +0300

    cw7

--- question1.js
+++ question1.js
@@ -1,1 +1,24 @@
 // قم بحل السؤال الأول هنا
+
+let pets = [`cat`, `dog`, `bird`, `fish`, `girrafe`]
+
+let newpet = prompt(`Whats your pet?`)
+
+pets.pop();
+
+pets.push(newpet)
+
+pets.splice(2, 1)
+
+console.log(pets)
+
+let object = {
+    name:`kitty`,
+    age:`2`,
+    type:`mammal`,
+    emoji: `🐱`
+}
+
+console.log(object)
+
+alert(object.age)
